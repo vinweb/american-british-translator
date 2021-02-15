110bb14a656a3c0cd763bc334b8f521da95a8f2d
Vincze János <vinczit@gmail.com>
2021-02-15 15:08:23 +0100

    added time handling

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,28 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
-  const translator = new Translator();
+    const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
+    app.route("/api/translate").post((req, res) => {
+        let text = req.body.text;
+        let locale = req.body.locale;
+        if (locale == "american-to-british") {
+            let translationText = text.replace(":", ".");
+            let translation = `<span class="highlight">${translationText}</span>`;
+            return res.json({
+                text: text,
+                translation: translation,
+            });
+        }
+        if (locale == "british-to-american") {
+            let translationText = text.replace(".", ":");
+            let translation = `<span class="highlight">${translationText}</span>`;
+            return res.json({
+                text: text,
+                translation: translation,
+            });
+        }
     });
 };
