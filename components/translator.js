f9e395adfeb5367652ff0412c86f5864aa62b794
Vincze János <vinczit@gmail.com>
2021-02-15 17:41:19 +0100

    added search and regex

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,36 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
 class Translator {
-
+    amToBrit(text) {
+        let textArray = text.split(" ");
+        let regexTime = /^\d\d:\d\d$/;
+        let amToBritArray = Object.keys(americanToBritishSpelling);
+        let translatedArray = [];
+        for (const i of textArray) {
+            let found = amToBritArray.find((e) => e === i);
+            if (found) {
+                let britWord = americanToBritishSpelling[found];
+                translatedArray.push(
+                    `<span class="highlight">${britWord}</span>`
+                );
+            } else if (regexTime.test(i)) {
+                let britTime = i.replace(":", ".");
+                translatedArray.push(
+                    `<span class="highlight">${britTime}</span>`
+                );
+            } else {
+                translatedArray.push(i);
+            }
+        }
+        let translation = translatedArray.join(" ");
+        return {
+            text: text,
+            translation: translation,
+        };
+    }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -8,15 +8,24 @@ module.exports = function (app) {
     app.route("/api/translate").post((req, res) => {
         let text = req.body.text;
         let locale = req.body.locale;
-        if (locale == "american-to-british") {
-            let translationText = text.replace(":", ".");
+
+        //let regex = /[\d]{2}[.:][\d]{2}/;
+        if (Object.keys(req.body).length < 2) {
+            return res.json({ error: "Required field(s) missing" });
+        }
+        if (text == "") {
+            return res.json({ error: "No text to translate" });
+        }
+        if (locale == "american-to-british" && text) {
+            /* let translationText = text.replace(":", ".");
             let translation = `<span class="highlight">${translationText}</span>`;
             return res.json({
                 text: text,
                 translation: translation,
-            });
+            }); */
+            return res.json(translator.amToBrit(text));
         }
-        if (locale == "british-to-american") {
+        if (locale == "british-to-american" && text) {
             let translationText = text.replace(".", ":");
             let translation = `<span class="highlight">${translationText}</span>`;
             return res.json({
